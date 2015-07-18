58a9ac7e3c64dee542a71fa745130f270cedf106
daz2yy <xiaodatc@126.com>
2015-07-18 15:09:05 +0800

    Project RenameFile and DragAndRun
    Project RenameFile and DragAndRun

---
+++ DragAndRun/DragAndRun/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragAndRun
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_Drag(object sender, DragEventArgs e)
+        {
+            string msg = "Drop";
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                System.Array array = ((System.Array)e.Data.GetData(DataFormats.FileDrop));
+                msg = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            }
+            if (msg.Contains(".lua"))
+            {
+                msg = "This is a lua file:" + msg;
+            }
+            else if (msg.Contains(".png") || msg.Contains(".jpg"))
+            {
+                msg = "This is a image file:" + msg;
+            }
+
+            //Process myProcess = new Process();
+            //string fileName = @"D:\PngEncode.exe";
+            //string para = @"-i " + msg + @" -o " + msg;
+            //ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(fileName, para);
+            //myProcess.StartInfo = myProcessStartInfo;
+            //myProcess.Start();
+            //while (!myProcess.HasExited)
+            //{
+            //   myProcess.WaitForExit();
+            //}
+
+            MessageBox.Show("finish encodeing");
+        }
+    }
+}
---
+++ ReNameFile/ReNameFile/View/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReNameFile
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private bool bIslegal()
+        {
+            if (String.IsNullOrEmpty(Address.Text)
+                || String.IsNullOrEmpty(From.Text)
+                || String.IsNullOrEmpty(To.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+        private void ReplaceFile(object sender, RoutedEventArgs e)
+        {
+            //Address.Text = @"D:\Study\C#\ReNameFile\text";
+            //From.Text = @"2";
+            //To.Text = @"123";
+            if (!bIslegal())
+            {
+                return;
+            }
+            DirectoryInfo TheFolder = new DirectoryInfo(Address.Text);
+            foreach (FileInfo item in TheFolder.GetFiles())
+            {
+                String fullPath = item.FullName.Replace("/", @"\");
+                String path = fullPath.Substring(0, fullPath.LastIndexOf(@"\")+1);
+                String extension = System.IO.Path.GetExtension(item.Name);
+                String fileName = System.IO.Path.GetFileNameWithoutExtension(item.Name);
+                String newName = fileName.Replace(From.Text, To.Text);
+                if (fileName != newName)
+                {
+                    File.Move(item.FullName, path + newName + extension);
+                }
+            }
+        }
+
+        private void ReplaceFolder(object sender, RoutedEventArgs e)
+        {
+            if (!bIslegal())
+            {
+                return;
+            }
+            DirectoryInfo TheFolder = new DirectoryInfo(Address.Text);
+            foreach (DirectoryInfo item in TheFolder.GetDirectories())
+            {
+                String fullPath = item.FullName.Replace("/", @"\");
+                String path = fullPath.Substring(0, fullPath.LastIndexOf(@"\") + 1);
+                String newName = item.Name.Replace(From.Text, To.Text);
+                if (item.Name != newName)
+                {
+                    Directory.Move(item.FullName, path + newName);
+                }
+            }
+        }
+    }
+}
